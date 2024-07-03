689779150f930297801ad94dd368a7b21a200062
Sukrit Priyank <98573534+sukrit890@users.noreply.github.com>
2024-07-03 23:36:30 +0530

    Time: 7 ms (19.24%), Space: 13.4 MB (20.34%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int count = 0;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            count++;
-            temp = temp->next;
+        //striver video solution optimal approach
+        ListNode* fast = head;
+        ListNode* slow = head;
+        for(int i = 0;i<n;i++){
+            fast = fast -> next;
         }
-        if(n == count){
-            head=head->next;
-            return head;
+        if(fast == NULL) return head->next;
+
+        while(fast->next != NULL){
+            fast = fast->next;
+            slow = slow->next;
         }
-        temp = head;
-        for(int i=1;i<(count-n);i++){
-            temp = temp->next;
-        }
-        temp->next = temp->next->next;
+        ListNode* delNode = slow -> next;
+        slow ->next = slow->next->next;
+        delete delNode;
         return head;
     }
 };
