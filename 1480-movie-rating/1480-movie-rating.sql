fe8adbb798ad4136dfaedabfad7dd932d24f22dc
Sukrit Priyank <98573534+sukrit890@users.noreply.github.com>
2024-08-29 01:08:47 +0530

    Time: 1793 ms (6.92%), Space: 0B (100%) - LeetHub

--- 1480-movie-rating/1480-movie-rating.sql
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -1,19 +1,31 @@
-# Write your MySQL query statement below
-(select name as results
-from users as u
- join movierating as m using(user_id)
- 
- group by user_id
- order by count(rating) desc, name
- limit 1)
-
-UNION all 
-(
-select title as results
-from movies m
-join movierating as mr using(movie_id)
-where month(created_at)='02' and year(created_at)='2020'
-group by title
-order by avg(rating)desc, title
-limit 1
+(select u.name as results
+from users u 
+join movierating m on u.user_id=m.user_id
+group by u.user_id
+having count(m.rating)=(
+    select count(rating)
+    from movierating
+    group by user_id
+    order by count(rating) desc
+    limit 1
 )
+order by u.name asc 
+limit 1)
+
+union all
+(
+select mo.title as results
+from movies mo
+join movierating m on mo.movie_id=m.movie_id
+where extract(year_month from m.created_at)=202002
+group by mo.movie_id
+having avg(rating)=(
+    select avg(rating)
+    from movierating
+    where extract(year_month from created_at)=202002
+    group by movie_id
+    order by avg(rating) desc
+    limit 1
+)
+order by mo.title asc
+limit 1)
